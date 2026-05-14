d1ba8b89f3b1fe8420a1120bd6d7f7e73eb55c5d
bassaaaa <white.galaxys2.album2@gmail.com>
2026-05-14 23:38:38 +0900

    ex3を追加

---
+++ ex3/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total() -> None:
+    day1 = int(input("Day1 harvest: "))
+    day2 = int(input("Day2 harvest: "))
+    day3 = int(input("Day3 harvest: "))
+    print("Total harvest:", day1 + day2 + day3)
+

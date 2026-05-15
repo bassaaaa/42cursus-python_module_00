8a5b97b4114a4aa400de8d72d596003c17ca7879
bassaaaa <white.galaxys2.album2@gmail.com>
2026-05-16 00:22:39 +0900

    flake8違反を修正

--- ex3/ft_harvest_total.py
+++ ex3/ft_harvest_total.py
@@ -1,6 +1,5 @@
 def ft_harvest_total() -> None:
-    day1 = int(input("Day1 harvest: "))
-    day2 = int(input("Day2 harvest: "))
-    day3 = int(input("Day3 harvest: "))
+    day1 = int(input("Day 1 harvest: "))
+    day2 = int(input("Day 2 harvest: "))
+    day3 = int(input("Day 3 harvest: "))
     print("Total harvest:", day1 + day2 + day3)
-
